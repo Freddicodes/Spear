182e08e40e933f577b7c0eb89f5f62ae3b9c21d9
Jan Frederik Draeger <jan.draeger@student.uni-augsburg>
2024-02-05 16:07:17 +0100

    Adds ability to switch to application window with keypress

--- CheckShortcutInterface.py
+++
@@ -1,3 +0,0 @@
-class CheckShortcutInterface:
-    def is_valid(self, shortcut):
-        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from pynput import keyboard
-from CheckShortcutSet import CheckShortcutSet
+from AppKit import NSWorkspace, NSRunningApplication, NSApplicationActivateIgnoringOtherApps
+
+key_to_app = {
+    'a': 'com.apple.Safari',
+    'b': 'com.apple.TextEdit',
+}
+
+active_keys = set()
+
+
+def on_press(key):
+    active_keys.add(key)
+
+
+def on_release(key):
+    try:
+        if isinstance(key, keyboard.KeyCode) and key.char in key_to_app:
+            print(f"key: {key.char}")
+            app_bundle_id = key_to_app[key.char]
+            # running_apps = NSWorkspace.sharedWorkspace().runningApplications()
+            # app: NSRunningApplication = [x for x in running_apps if x.bundleIdentifier() == app_bundle_id]
+            app = NSRunningApplication.runningApplicationsWithBundleIdentifier_(app_bundle_id)
+            if app:
+                app[0].activateWithOptions_(NSApplicationActivateIgnoringOtherApps)
+        elif isinstance(key, keyboard.Key):
+            print(f"key: {key.name}")
+    except AttributeError as error:
+        print(f"error: {error}")
+    finally:
+        if key in active_keys:
+            active_keys.remove(key)
 
 
 def main():
-    strategy = CheckShortcutSet()
-
-    with keyboard.Listener(on_press=strategy.on_press, on_release=strategy.on_release) as listener:
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
 
